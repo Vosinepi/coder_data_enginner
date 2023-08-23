dbffbcd57fbe435db72b6a2499f125b61b8e2411
Vosinepi <ismaelpiovani@gmail.com>
2023-08-23 12:07:53 -0300

    Entrega_1_coder_enginner

---
+++ util/settings.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# El código utiliza la función `os.getenv()` para recuperar variables de entorno.
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_schema = "ismaelpiovani_coderhouse"
