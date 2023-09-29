5ff3e89f75d9aa5bfab866ac68b133f4e3556186
Vosinepi <ismaelpiovani@gmail.com>
2023-09-29 15:15:35 -0300

    1

--- airflow_coder/dags/scripts/db.py
+++ airflow_coder/dags/scripts/db.py
@@ -4,8 +4,6 @@ sys.path.append(".")
 
 from psycopg2 import connect
 
-# from .settings import db_name, db_user, db_password, db_host, db_port
-
 
 # Conecto a la base de datos
 def ddbb_conection(db_name, db_user, db_password, db_host, db_port):
